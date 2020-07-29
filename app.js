7e8cce772922dec2ebccfbcb50d709c0c15d8b73
Guy <guyspriggs23@gmail.com>
2020-07-29 18:55:39 +0200

    fix landing page

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ app.use("/", authRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP , function(){
+
+app.listen(3000, process.env.IP , function(){
     console.log("CampSites server has started!");
 });
 
