26bb37996f71d3ccf8c81e2456deb228b7bc9ea5
Guy <guyspriggs23@gmail.com>
2020-07-29 18:00:33 +0200

    fix deployment

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use("/", authRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.port, process.env.IP , function(){
+app.listen(process.env.PORT, process.env.IP , function(){
     console.log("CampSites server has started!");
 });
 
