fd3ebf53b61fbbc0dc259be87e728b7bc5bb9cbc
Guy <guyspriggs23@gmail.com>
2020-07-29 19:05:12 +0200

    responsive images

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP , function(){
+app.listen(process.env.PORT , process.env.IP , function(){
     console.log("CampSites server has started!");
 });
 
